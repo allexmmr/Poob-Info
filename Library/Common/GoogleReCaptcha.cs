3cb2a04755c7d25b471aa8199edab5a8c1cc2b13
Allex Rocha <allex@jxt.com.au>
2020-02-11 23:51:26 +1100

    Updated Google reCAPTCHA

--- Library/Common/GoogleReCaptcha.cs
+++ Library/Common/GoogleReCaptcha.cs
@@ -1,19 +1,37 @@
 ﻿using Newtonsoft.Json.Linq;
+using System.Configuration;
+using System.IO;
 using System.Net;
+using System.Web;
 
-public static class GoogleReCaptcha
+namespace Library.Common
 {
-    public static bool Validate(string siteSecret, string response)
+    public static class GoogleReCaptcha
     {
-        bool valid = false;
+        public static bool Validate(out string errorMessage)
+        {
+            errorMessage = null;
+            bool isValid = false;
+            string captchaResponse = HttpContext.Current.Request.Form["g-recaptcha-response"];
+            captchaResponse = captchaResponse?.TrimEnd(',');
+            string secretKey = ConfigurationManager.AppSettings["SecretKey"];
+            string apiUrl = "https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}";
+            string requestUri = string.Format(apiUrl, secretKey, captchaResponse);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(requestUri);
 
-        WebClient webClient = new WebClient();
-        string json = webClient.DownloadString($"https://www.google.com/recaptcha/api/siteverify?secret={siteSecret}&response={response}");
-        JObject obj = JObject.Parse(json);
+            using (WebResponse response = request.GetResponse())
+            {
+                using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+                {
+                    JObject jResponse = JObject.Parse(stream.ReadToEnd());
+                    isValid = jResponse.Value<bool>("success") ? true : false;
 
-        if (obj["success"].Value<bool>())
-            valid = true;
+                    if (!isValid)
+                        errorMessage = "Falha ao validar o captcha.";
+                }
+            }
 
-        return valid;
+            return isValid;
+        }
     }
 }
